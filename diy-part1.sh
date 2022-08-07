c7273dab37ef6b1e494af737dfd97770eef3c0d5
cykcoco <96184463+cykcoco@users.noreply.github.com>
2022-08-07 11:56:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,5 @@
 echo 'src-git argon https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo 'src-git openAppFilter https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
+echo 'src-git oaf https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
+echo 'src-git alist https://github.com/sbwml/luci-app-alist' >>feeds.conf.default
