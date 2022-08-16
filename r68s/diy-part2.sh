12fe9037d94d9807500e17d45530d20635326005
cykcoco <96184463+cykcoco@users.noreply.github.com>
2022-08-16 17:04:47 +0800

    Update diy-part2.sh

--- r68s/diy-part2.sh
+++ r68s/diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git apply 0002-rockchip-add-FastRhino-R68S-support-in-kernel-5.19-9.patch
