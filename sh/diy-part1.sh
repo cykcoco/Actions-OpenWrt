c4154e97bface548e429ba7fe8e5a98b16b90adf
cykcoco <96184463+cykcoco@users.noreply.github.com>
2024-02-17 19:35:38 +0800

    Update diy-part1.sh

--- sh/diy-part1.sh
+++ sh/diy-part1.sh
@@ -5,7 +5,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git argon https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
+# echo 'src-git argon https://github.com/jerrykuku/openwrt-package' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 echo 'src-git oaf https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
